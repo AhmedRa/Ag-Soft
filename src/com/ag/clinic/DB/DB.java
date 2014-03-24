a72b220c93884d0579946e3dec35c0a99ca4754e
ahmedshalash114 <ashalash38@gmail.com>
2014-03-24 19:06:10 +0200

    ahmed shalash first commit

---
+++ src/com/ag/clinic/DB/DB.java
@@ -0,0 +1,13 @@
+package com.ag.clinic.DB;
+
+import java.sql.Connection;
+
+public class DB {
+    
+    public Connection getMySqlConn(){ 
+        
+        return new connections.DBConnection().getMysqlDriver("localhost", "clinic?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8", "root", null) ;
+      
+    }
+   
+}
